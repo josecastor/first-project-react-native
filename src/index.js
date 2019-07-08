c35acfa3050cc15a49a2529ce545c196b52c60b3
José Castor <junior.castor@gmail.com>
2019-07-08 16:26:31 -0300

    React navigation

--- src/index.js
+++ src/index.js
@@ -1,46 +1,12 @@
-import React, { Fragment } from 'react';
-import { SafeAreaView, StyleSheet, ScrollView, StatusBar } from 'react-native';
+import React from 'react';
+// import { View } from 'react-native';
+
+import Routes from './routes';
 
 import './config/ReactotronConfig';
 
-import { Header, Colors } from 'react-native/Libraries/NewAppScreen';
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-console.tron.warn('Hello World fdfsfsf');
 const App = () => {
-  return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}
-        >
-          <Header />
-        </ScrollView>
-      </SafeAreaView>
-    </Fragment>
-  );
+  return <Routes />;
 };
 
 export default App;
